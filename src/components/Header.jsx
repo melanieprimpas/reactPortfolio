684937dc4c184f414a44bb2eca6330012685db65
melanieprimpas <melanie.primpas@gmail.com>
2024-10-10 14:35:28 -0400

    created initial code with react router. Now have to update pages and update css

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import NavTabs from './NavTabs';
-
-export default function Header({ currentPage, handlePageChange }) {
-    return (
-        <header>
-            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-        </header>
-    );
-}
